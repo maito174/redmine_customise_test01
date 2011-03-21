dbe9fdd0ac4e05e1cc25b36ca1953d190c1b09f1
S.Komatsu <shigeru.komatsu@gmail.com>
2011-03-21 22:33:37 +0900

    minor change of redmine_start.bat

--- redmine_start.bat
+++ redmine_start.bat
@@ -1,5 +1,5 @@
-cd c:\dev\ruby\redmine
+cd c:/dev/ruby/redmine
 ruby script/server webrick -e development
 
-# ログをファイル出力
+# ログをファイル出力 ( Log to File)
 # ruby script/server webrick -e development > redmine_start.log 2>&1 
